2228b4e8601a26cabfe782e7bac33b59ccf2a057
deepakviswanadh <deepak16c@gmail.com>
2024-04-11 16:55:00 -0400

    added py constructor files

--- src/main/java/org/example/GraphAdapter/GraphAdapter.java
+++ src/main/java/org/example/GraphAdapter/GraphAdapter.java
@@ -1,6 +1,5 @@
 package org.example.GraphAdapter;
-import org.jgrapht.ext.;
 import org.example.GraphNode.GraphNode;
 
-public class GraphAdapter implements VertexNameProvider<GraphNode>{
+public class GraphAdapter implements GraphVisualNode{
 }
